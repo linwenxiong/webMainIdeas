5ef9295f97d33ecde359cc3027bf6b1b83eadbfd
林文雄 <U1000509>
2024-11-01 18:08:11 +0800

    feat:添加getclass方法

--- class.js
+++ class.js
@@ -1,39 +1,30 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Bbq = /** @class */ (function () {
-    function Bbq() {
-        this.str = '我是str';
+// class Bbq {
+//   public str: string;
+//   constructor() {
+//     this.str = '我是str'
+//   }
+//   getData() {
+//     console.log('234243243242',this.str)
+//   }
+// }
+// class Editor extends Bbq {
+//   private canvas = 'null';
+//   constructor() {
+//     super()
+//     this.canvas = '大家好'
+//   }
+//   init() {
+//     console.log('canvas', this.canvas)
+//   }
+// }
+// var a = new Editor()
+// a.getData()
+var Audios = /** @class */ (function () {
+    function Audios() {
+        this.audio = '我是audio';
     }
-    Bbq.prototype.getData = function () {
-        console.log('234243243242', this.str);
+    Audios.prototype.getData = function () {
+        console.log('audio', this.audio);
     };
-    return Bbq;
+    return Audios;
 }());
-var Editor = /** @class */ (function (_super) {
-    __extends(Editor, _super);
-    function Editor() {
-        var _this = _super.call(this) || this;
-        _this.canvas = 'null';
-        _this.canvas = '大家好';
-        return _this;
-    }
-    Editor.prototype.init = function () {
-        console.log('canvas', this.canvas);
-    };
-    return Editor;
-}(Bbq));
-var a = new Editor();
-a.getData();
--- class.ts
+++ class.ts
@@ -1,25 +1,49 @@
 
-class Bbq {
-  public str: string;
+// class Bbq {
+//   public str: string;
+//   constructor() {
+//     this.str = '我是str'
+//   }
+//   getData() {
+//     console.log('234243243242',this.str)
+//   }
+// }
+
+// class Editor extends Bbq {
+//   private canvas = 'null';
+//   constructor() {
+//     super()
+//     this.canvas = '大家好'
+//   }
+  
+//   init() {
+//     console.log('canvas', this.canvas)
+//   }
+// }
+
+// var a = new Editor()
+// a.getData()
+
+
+class Audios {
+  private audio: string;
+  public name: string;
   constructor() {
-    this.str = '我是str'
+    this.audio = '我是audio'
   }
   getData() {
-    console.log('234243243242',this.str)
+    console.log('audio', this.audio)
   }
 }
 
-class Editor extends Bbq {
-  private canvas = 'null';
+class Music extends Audios {
+  private music: string;
   constructor() {
     super()
-    this.canvas = '大家好'
+    this.music = '我是music'
   }
   
   init() {
-    console.log('canvas', this.canvas)
+    console.log('music', this.music)
   }
 }
-
-var a = new Editor()
-a.getData()
